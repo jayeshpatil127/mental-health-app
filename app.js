e9b5e649d33963df78e0a2e8d1e7b54a75d914d1
jayesh patil <jayeshpatil7215@gmail.com>
2025-12-25 20:30:10 +0530

    Fix case-sensitive route imports for Linux

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.use((req, res, next) => {
 });
 
 // ================== ROUTES ==================
-app.use("/checkin", require("./routes/dailycheckin"));
+app.use("/checkin", require("./routes/DailyCheckin"));
 app.use("/api", require("./routes/riskRoutes"));
 app.use("/", require("./routes/dashboard"));
 app.use("/", require("./routes/profile"));
